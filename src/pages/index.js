087cc8f7d890a372ca8de908d1ebd03cd9523da9
Caity Gossland <caitygossland@gmail.com>
2022-05-11 10:19:52 -0700

    Initial commit - allow dynamic products from GraphCMS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
-import Head from 'next/head'
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
-import Layout from '@components/Layout';
-import Container from '@components/Container';
-import Button from '@components/Button';
+import Layout from "@components/Layout";
+import Container from "@components/Container";
+import Button from "@components/Button";
 
-import products from '@data/products';
+import products from "@data/products";
 
-import styles from '@styles/Page.module.scss'
+import styles from "@styles/Page.module.scss";
 
-export default function Home() {
+export default function Home({ home, products }) {
+  const { heroTitle, heroText, heroLink, heroBackground } = home;
+  console.log(products);
   return (
     <Layout>
       <Head>
@@ -21,13 +24,19 @@ export default function Home() {
         <h1 className="sr-only">Space Jelly Gear</h1>
 
         <div className={styles.hero}>
-          <Link href="#">
+          <Link href={heroLink}>
             <a>
               <div className={styles.heroContent}>
-                <h2>Prepare for liftoff.</h2>
-                <p>Apparel that&apos;s out of this world!</p>
+                <h2>{heroTitle}</h2>
+                <p>{heroText}</p>
               </div>
-              <img className={styles.heroImage} src="/images/space-jelly-gear-banner.jpg" alt="" />
+              <img
+                className={styles.heroImage}
+                width={heroBackground.width}
+                height={heroBackground.height}
+                src={heroBackground.url}
+                alt=""
+              />
             </a>
           </Link>
         </div>
@@ -35,32 +44,79 @@ export default function Home() {
         <h2 className={styles.heading}>Featured Gear</h2>
 
         <ul className={styles.products}>
-          {products.slice(0, 4).map(product => {
+          {products.map((product) => {
             return (
-              <li key={product.id}>
+              <li key={product.slug}>
                 <Link href="#">
                   <a>
                     <div className={styles.productImage}>
-                      <img width="500" height="500" src={product.image} alt="" />
+                      <img
+                        width={product.image.width}
+                        height={product.image.height}
+                        src={product.image.url}
+                        alt=""
+                      />
                     </div>
-                    <h3 className={styles.productTitle}>
-                      { product.name }
-                    </h3>
-                    <p className={styles.productPrice}>
-                      ${ product.price }
-                    </p>
+                    <h3 className={styles.productTitle}>{product.name}</h3>
+                    <p className={styles.productPrice}>${product.price}</p>
                   </a>
                 </Link>
                 <p>
-                  <Button>
-                    Add to Cart
-                  </Button>
+                  <Button>Add to Cart</Button>
                 </p>
               </li>
-            )
+            );
           })}
         </ul>
       </Container>
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: "https://api-us-west-2.graphcms.com/v2/cl1wtxv4f0jx901z17q1u82bs/master",
+    cache: new InMemoryCache(),
+  });
+
+  const data = await client.query({
+    query: gql`
+      query PageHome {
+        page(where: { slug: "home" }) {
+          id
+          heroLink
+          heroText
+          heroTitle
+          name
+          slug
+          stage
+          heroBackground {
+            url
+            height
+            width
+          }
+        }
+        products(first: 4) {
+          name
+          price
+          slug
+          image {
+            height
+            url
+            width
+          }
+        }
+      }
+    `,
+  });
+
+  const home = data.data.page;
+  const products = data.data.products;
+
+  return {
+    props: {
+      home,
+      products,
+    },
+  };
 }
